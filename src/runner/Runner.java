345616d07d22218f309aa88311861f37c72db018
Diego Marcilio <dvmarcilio@gmail.com>
2018-06-02 00:22:13 -0300

    Runner refactoring for multiple paths given as arguments

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -19,36 +19,42 @@ public class Runner {
 	private static final String MUTABLE_MEMBERS_USAGE_REFACTORER_MODULE = "rjtl::MutableMembersUsage";
 
 	public static void main(String[] args) throws URISyntaxException {
-		checkIfArgsAreValid(args);
-		
+		ArgsValidator.validate(args);
+
 		Evaluator evaluator = ShellEvaluatorFactory.getDefaultEvaluator(new PrintWriter(System.out),
 				new PrintWriter(System.err));
 
 		evaluator.doImport(evaluator.getMonitor(), MUTABLE_MEMBERS_USAGE_REFACTORER_MODULE);
 
-		Path fileLocation = Paths.get(args[0]);
+		for (String path : args) {
+			refactorForMutableMembersUsage(evaluator, path);
+		}
+	}
+
+	private static void refactorForMutableMembersUsage(Evaluator evaluator, String path) throws URISyntaxException {
+		Path fileLocation = Paths.get(path);
 		ISourceLocation fileLoc = URIUtil.createFileLocation(fileLocation.toString());
 		evaluator.call("refactorMutableGettersAndSettersViolations", fileLoc);
-
 	}
 
-	private static void checkIfArgsAreValid(String[] args) {
-		if(args.length == 0)
-			throw new IllegalArgumentException("At least one path should be given as argument");
-		
-		if(!checkIfValidFiles(args))
-			throw new IllegalArgumentException("All paths should exist, be a file (not a directory) and be readable/writable");
-		
-	}
-	
-	private static boolean checkIfValidFiles(String[] args) {
-		Predicate<Path> exists = Files::exists;
-		Predicate<Path> isDirectory = Files::isDirectory;
-		Predicate<Path> isNotDirectory = isDirectory.negate();
-		Predicate<Path> isReadable = Files::isReadable;
-		Predicate<Path> isWritable = Files::isWritable;
-		Predicate<Path> validFile = exists.and(isNotDirectory).and(isReadable).and(isWritable);
-		return Stream.of(args).map(Paths::get).allMatch(validFile);
-	}
+	private static class ArgsValidator {
+		private static void validate(String[] args) {
+			if (args.length == 0)
+				throw new IllegalArgumentException("At least one path should be given as argument");
 
+			if (!checkIfValidFiles(args))
+				throw new IllegalArgumentException(
+						"All paths should exist, be a file (not a directory) and be readable/writable");
+		}
+
+		private static boolean checkIfValidFiles(String[] args) {
+			Predicate<Path> exists = Files::exists;
+			Predicate<Path> isDirectory = Files::isDirectory;
+			Predicate<Path> isNotDirectory = isDirectory.negate();
+			Predicate<Path> isReadable = Files::isReadable;
+			Predicate<Path> isWritable = Files::isWritable;
+			Predicate<Path> validFile = exists.and(isNotDirectory).and(isReadable).and(isWritable);
+			return Stream.of(args).map(Paths::get).allMatch(validFile);
+		}
+	}
 }
