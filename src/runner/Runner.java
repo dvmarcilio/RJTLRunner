4c75d863b13d4285970cbda3e64fad3274aa4354
Diego Marcilio <dvmarcilio@gmail.com>
2018-06-02 00:10:10 -0300

    Checking if args passed to Runner.main() are valid

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -2,8 +2,11 @@ package runner;
 
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.shell.ShellEvaluatorFactory;
@@ -16,6 +19,8 @@ public class Runner {
 	private static final String MUTABLE_MEMBERS_USAGE_REFACTORER_MODULE = "rjtl::MutableMembersUsage";
 
 	public static void main(String[] args) throws URISyntaxException {
+		checkIfArgsAreValid(args);
+		
 		Evaluator evaluator = ShellEvaluatorFactory.getDefaultEvaluator(new PrintWriter(System.out),
 				new PrintWriter(System.err));
 
@@ -25,6 +30,25 @@ public class Runner {
 		ISourceLocation fileLoc = URIUtil.createFileLocation(fileLocation.toString());
 		evaluator.call("refactorMutableGettersAndSettersViolations", fileLoc);
 
+	}
+
+	private static void checkIfArgsAreValid(String[] args) {
+		if(args.length == 0)
+			throw new IllegalArgumentException("At least one path should be given as argument");
+		
+		if(!checkIfValidFiles(args))
+			throw new IllegalArgumentException("All paths should exist, be a file (not a directory) and be readable/writable");
+		
+	}
+	
+	private static boolean checkIfValidFiles(String[] args) {
+		Predicate<Path> exists = Files::exists;
+		Predicate<Path> isDirectory = Files::isDirectory;
+		Predicate<Path> isNotDirectory = isDirectory.negate();
+		Predicate<Path> isReadable = Files::isReadable;
+		Predicate<Path> isWritable = Files::isWritable;
+		Predicate<Path> validFile = exists.and(isNotDirectory).and(isReadable).and(isWritable);
+		return Stream.of(args).map(Paths::get).allMatch(validFile);
 	}
 
 }
