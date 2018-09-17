19f3aa297695cd45a080973a6b87ae2b1669ff61
Diego Marcilio <dvmarcilio@gmail.com>
2018-09-16 23:19:58 -0300

    removeUnusedImports from google-java-formatter

--- README.md
+++ README.md
@@ -13,18 +13,18 @@ mvn clean compile assembly:single
 
 This will create `target/RJTLRunner-x.x.x-SNAPSHOT-jar-with-dependencies`
 
-With the JAR successfully created, we can pass several .java file paths to the runner to refactor for MutableMembersUsage.
+With the JAR successfully created, we can pass several .java file paths to the runner to refactor for MutableMembersUsage and RemoveUnusedImports.
 
 For instance:
 
 ```bash
-java -jar .\RJTLRunner-0.0.1-SNAPSHOT-jar-with-dependencies.jar C:\dev\MutableMembersUsage\FuncaoDadosVersionavel.java C:\d
+java -jar .\RJTLRunner-0.0.2-SNAPSHOT-jar-with-dependencies.jar C:\dev\MutableMembersUsage\FuncaoDadosVersionavel.java C:\d
 ev\MutableMembersUsage\FuncaoDados.java C:\dev\MutableMembersUsage\FuncaoTransacao.java
 ```
 
 We have to make sure that the JAR is run with a JDK. This was achieved on windows by running:
 
 ```bash
-C:\Progra~1\Java\jdk1.8.0_121\bin\java.exe -jar .\RJTLRunner-0.0.1-SNAPSHOT-jar-with-dependencies.jar C:\dev\MutableMembersUsage\FuncaoDadosVersionavel.java C:\d
+C:\Progra~1\Java\jdk1.8.0_121\bin\java.exe -jar .\RJTLRunner-0.0.2-SNAPSHOT-jar-with-dependencies.jar C:\dev\MutableMembersUsage\FuncaoDadosVersionavel.java C:\d
 ev\MutableMembersUsage\FuncaoDados.java C:\dev\MutableMembersUsage\FuncaoTransacao.java
 ```
--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -1,5 +1,6 @@
 package runner;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -9,8 +10,12 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.rascalmpl.interpreter.Evaluator;
+import org.rascalmpl.interpreter.control_exceptions.ControlException;
 import org.rascalmpl.shell.ShellEvaluatorFactory;
 import org.rascalmpl.uri.URIUtil;
+
+import com.google.googlejavaformat.java.FormatterException;
+import com.google.googlejavaformat.java.RemoveUnusedImports;
 
 import io.usethesource.vallang.ISourceLocation;
 
@@ -18,23 +23,55 @@ public class Runner {
 
 	private static final String MUTABLE_MEMBERS_USAGE_REFACTORER_MODULE = "rjtl::MutableMembersUsage";
 
-	public static void main(String[] args) throws URISyntaxException {
-		ArgsValidator.validate(args);
+	private static final Evaluator evaluator;
 
-		Evaluator evaluator = ShellEvaluatorFactory.getDefaultEvaluator(new PrintWriter(System.out),
-				new PrintWriter(System.err));
+	static {
+		evaluator = ShellEvaluatorFactory.getDefaultEvaluator(new PrintWriter(System.out), new PrintWriter(System.err));
 
 		evaluator.doImport(evaluator.getMonitor(), MUTABLE_MEMBERS_USAGE_REFACTORER_MODULE);
+	}
 
-		for (String path : args) {
-			refactorForMutableMembersUsage(evaluator, path);
+	public static void main(String[] args) throws URISyntaxException {
+		ArgsValidator.validate(args);
+		runTransformations(args);
+	}
+
+	private static void runTransformations(String[] paths) {
+		try {
+			doRunTransformations(evaluator, paths);
+		} catch (ControlException | URISyntaxException e) {
+			// ignoring exceptions, just proceed to next file
+			System.out.println();
 		}
 	}
 
-	private static void refactorForMutableMembersUsage(Evaluator evaluator, String path) throws URISyntaxException {
+	private static void doRunTransformations(Evaluator evaluator, String[] paths) throws URISyntaxException {
+		for (String path : paths) {
+			refactorForMutableMembersUsage(path);
+			organizeImports(path);
+		}
+	}
+
+	private static void refactorForMutableMembersUsage(String path) throws URISyntaxException {
 		Path fileLocation = Paths.get(path);
 		ISourceLocation fileLoc = URIUtil.createFileLocation(fileLocation.toString());
 		evaluator.call("refactorMutableGettersAndSettersViolations", fileLoc);
+	}
+
+	private static void organizeImports(String path) {
+		try {
+			doOrganizeImports(path);
+		} catch (IOException | FormatterException e) {
+			System.out.println();
+			// ignore
+		}
+	}
+
+	private static void doOrganizeImports(String pathStr) throws IOException, FormatterException {
+		Path path = Paths.get(pathStr);
+		String contents = new String(Files.readAllBytes(path));
+		String classWithoutUnusedImports = RemoveUnusedImports.removeUnusedImports(contents);
+		Files.write(path, classWithoutUnusedImports.getBytes());
 	}
 
 	private static class ArgsValidator {
